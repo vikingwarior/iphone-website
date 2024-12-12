1fbc6666974286f25544b6f38b7debe31405e106
Harsh Paradkar <contact.harshparadkar@gmail.com>
2024-12-13 01:11:41 +0530

    adds model component

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import Hero from "./components/Hero";
 import Highlights from "./components/Highlights";
+import Model from "./components/Model";
 import Navbar from "./components/Navbar";
 
 export default function App() {
@@ -8,6 +9,7 @@ export default function App() {
       <Navbar/>
       <Hero/>
       <Highlights/>
+      <Model />
     </main>
   )
 }
